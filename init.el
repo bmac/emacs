80b45891d9b963e7f51102ec07164eeed3d4d313
bmac <bmac325@GMail.com>
2009-07-20 22:58:27 -0400

    added tabbar.el

--- init.el
+++ init.el
@@ -67,3 +67,9 @@
 
 (load "light-symbol.el")
 
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(erc-modules (quote (autojoin button completion fill irccontrols log match menu netsplit noncommands readonly ring scrolltobottom stamp spelling track))))
