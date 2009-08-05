c2bf1b191e704f94536180808ee9791ad3cff5e0
bmac <bmac325@GMail.com>
2009-08-04 22:26:54 -0400

    added transparency function for emacs23 and smart-advance function

--- bmac/bmac-emacs23.el
+++ bmac/bmac-emacs23.el
@@ -1,2 +1,7 @@
 ;; Use system trash (for emacs 23)
 (setq delete-by-moving-to-trash t)
+
+(defun bmac-set-transparency (n)
+  "Sets the transparency of the current buffer. 100 for solid 0 for near see though."
+  (interactive "nAlpha (100 for a solid background):")
+  (modify-frame-parameters nil (list (cons 'alpha n))))
