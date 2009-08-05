0727af3bf1591c3cbcd9eb4793b9131a525dfcf0
bmac <bmac325@GMail.com>
2009-08-04 22:28:51 -0400

    ignore case for filenames in emacs23

--- bmac/bmac-emacs23.el
+++ bmac/bmac-emacs23.el
@@ -1,6 +1,9 @@
 ;; Use system trash (for emacs 23)
 (setq delete-by-moving-to-trash t)
 
+(setq read-buffer-completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+
 (defun bmac-set-transparency (n)
   "Sets the transparency of the current buffer. 100 for solid 0 for near see though."
   (interactive "nAlpha (100 for a solid background):")
