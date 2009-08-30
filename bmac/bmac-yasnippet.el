da6997eb4a6eb4c66097cbf12d54bf9547264463
bmac <bmac@Janissary.(none)>
2009-08-30 18:05:59 -0400

    auto-complete now works in org mode

--- bmac/bmac-yasnippet.el
+++ bmac/bmac-yasnippet.el
@@ -1,8 +1,13 @@
 
 ;;yasnippet http://code.google.com/p/yasnippet/
+
 (require 'yasnippet)
+(require 'dropdown-list)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/snippets")
 ;;(define-key yas/keymap [tab] 'yas/expand) 
+(setp yas/prompt-functions '(yas/ido-prompt 
+			     yas/dropdown-prompt 
+			     yas/no-prompt))
 
-
+(setp yas/trigger-key '("TAB" "SPC"))
