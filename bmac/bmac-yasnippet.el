8e560ac463c954ae09e9442e905c5e440567e1fa
bmac <bmac@Janissary.(none)>
2009-08-30 18:10:23 -0400

    lets try useing ido to set pick yas expansions

--- bmac/bmac-yasnippet.el
+++ bmac/bmac-yasnippet.el
@@ -6,8 +6,6 @@
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/snippets")
 ;;(define-key yas/keymap [tab] 'yas/expand) 
-(setp yas/prompt-functions '(yas/ido-prompt 
+(setq yas/prompt-functions '(yas/ido-prompt 
 			     yas/dropdown-prompt 
 			     yas/no-prompt))
-
-(setp yas/trigger-key '("TAB" "SPC"))
