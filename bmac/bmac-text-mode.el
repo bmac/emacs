76d9ceab85905c9e1e30aa73ff615f23ed59f95a
Brendan McLoughlin <bmac325@gmail.com>
2010-09-14 12:20:13 -0400

    turn off auto fill mode by default. its annoying

--- bmac/bmac-text-mode.el
+++ bmac/bmac-text-mode.el
@@ -1,5 +1,5 @@
 
 ;;textmode hooks
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
+;;(add-hook 'text-mode-hook 'turn-on-auto-fill)
 (add-hook 'text-mode-hook 'turn-on-flyspell)
 (add-hook 'text-mode-hook 'abbrev-mode)
