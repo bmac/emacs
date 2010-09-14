8198630575414a4ec53801be12a187527a5495ef
Brendan McLoughlin <bmac325@gmail.com>
2010-09-14 15:38:36 -0400

    turn on menubar mode in OSX

--- bmac/bmac-settings.el
+++ bmac/bmac-settings.el
@@ -88,4 +88,5 @@
       (setq mac-option-key-is-meta nil)
       (setq mac-command-key-is-meta t)
       (setq mac-command-modifier 'meta)
-      (setq mac-option-modifier nil)))
+      (setq mac-option-modifier nil)
+      (menu-bar-mode 1)))
