6911c6b75b84d178e48104e77cd0920bbedb6db4
Brendan McLoughlin <bmac325@gmail.com>
2010-09-15 17:36:28 -0400

    use js2 mode by default

--- bmac/bmac-javascript.el
+++ bmac/bmac-javascript.el
@@ -1,3 +1,4 @@
 ;;http://steve-yegge.blogspot.com/2008/03/js2-mode-new-javascript-mode-for-emacs.html
 (load "js2-20090723b")
 (setq js2-use-font-lock-faces t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
