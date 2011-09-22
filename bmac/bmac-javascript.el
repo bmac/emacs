08fc64294fc6253524bc420ed3ae76915bda6c8a
bmac <b@brendanmcloughlin.com>
2011-09-21 23:17:17 -0400

    updated settings so I'm not fighting with js2 mode as much

--- bmac/bmac-javascript.el
+++ bmac/bmac-javascript.el
@@ -4,3 +4,16 @@
 (load "js2-20090723b")
 (setq js2-use-font-lock-faces t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(setq 
+ js2-auto-indent-p t
+ js2-basic-offset 2
+ js2-bounce-indent-p t
+ js2-cleanup-whitespace t
+ js2-enter-indents-newline t
+ js2-highlight-level 3
+ js2-indent-on-enter-key t
+ js2-mirror-mode nil
+ js2-mode-escape-quotes nil
+ js2-enter-indents-newline t
+ js2-indent-on-enter-key nil)
+
