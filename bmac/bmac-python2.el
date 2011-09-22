f9a9a6227d546c74d1dcfcd3c7b5fa3da7320825
bmac <b@brendanmcloughlin.com>
2011-09-21 23:15:28 -0400

    trying out ipython for autocomplete

--- bmac/bmac-python2.el
+++ bmac/bmac-python2.el
@@ -1,94 +1,54 @@
-(require 'pycomplete)
-(load-library "python")
+(require 'comint)
 
-(autoload 'python-mode "python-mode" "Python Mode." t)
+(require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-(setq interpreter-mode-alist
-      (cons '("python" . python-mode)
-interpreter-mode-alist)
-      python-mode-hook
-      '(lambda () (progn
-(set-variable 'py-indent-offset 4)
-(set-variable 'py-smart-indentation nil)
-(set-variable 'indent-tabs-mode nil)
-;;(highlight-beyond-fill-column)
-                    (define-key python-mode-map "\C-m" 'newline-and-indent)
-;(pabbrev-mode)
-;(abbrev-mode)
-)
-      )
-)
 
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
-(autoload 'pymacs-load "pymacs" nil t)
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(setq interpreter-mode-alist(cons '("python" . python-mode)
-                             interpreter-mode-alist))
+(require 'ipython)
 
 
-(setq longlines-show-hard-newlines t) ; displays "\" at the end of lines that wrap past the window's edge
-
-                                        ; camel case word navigation
-(when (boundp 'subword-mode)
-  (add-hook 'after-change-major-mode-hook '(lambda () (subword-mode 1))))
-
-                                        ; Bindings
-(defun delete-backward-indent (&optional arg)
-  "Erase a level of indentation, or 1 character"
-  (interactive "*P")
-  (if (= (current-column) 0)
-      (delete-backward-char 1)
-    (let ((n (mod (current-column) standard-indent)))
-      (if (looking-back (concat "^\s*" (make-string n 32)))
-          (if (= n 0)
-              (delete-backward-char standard-indent)
-            (delete-backward-char n))
-        (delete-backward-char 1)))))
-
-(defun newline-maybe-indent ()
-  "Like newline-and-indent, but doesn't indent if the previous line is blank"
-  (interactive "*")
-  (if (= (line-beginning-position) (line-end-position))
-      (newline)
-    (newline-and-indent)))
-
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (hs-minor-mode 1) ; code folding
-             (define-key python-mode-map (kbd "RET") 'newline-maybe-indent)
-             (define-key python-mode-map (kbd "DEL") 'delete-backward-indent)
-	     (define-key python-mode-map "\t" 'py-complete)
-	     (define-key python-mode-map [f1] 'py-complete-help-thing-at-point)
-	     (define-key python-mode-map "(" 'py-complete-electric-lparen)
-	     (define-key python-mode-map "," 'py-complete-electric-comma)
-	     (define-key python-mode-map [f2] 'py-complete-signature-expr)
-	     (define-key python-mode-map [f3] 'py-complete-help)
-))
-
-                                        ; On-the-fly spell checking
-(add-hook 'python-mode-hook '(lambda () (flyspell-prog-mode)))
-
-; On-the-fly pyflakes checking
-(require 'flymake-point) ; shows errors in the minibuffer when highlighted (http://bitbucket.org/brodie/dotfiles/src/tip/.emacs.d/plugins/flymake-point.el)
-;; requires pyflakes to be installed change next string to be pyflakes command
-(setq python-check-command "pyflakes-2.4")
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflakes-2.4" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-(add-hook 'python-mode-hook '(lambda () (flymake-mode 1)))
+(define-key comint-mode-map (kbd "M-") 'comint-next-input)
+(define-key comint-mode-map (kbd "M-") 'comint-previous-input)
+(define-key comint-mode-map [down] 'comint-next-matching-input-from-input)
+(define-key comint-mode-map [up] 'comint-previous-matching-input-from-input)
 
 
+(defun dss-ipython-completion-candidate ()
+  (let* ((ugly-return nil)
+         (sep ";")
+         (python-process (or (get-buffer-process (current-buffer))
+                                        ;XXX hack for .py buffers
+                             (get-process py-which-bufname)))
+         (beg (save-excursion (skip-chars-backward "a-z0-9A-Z_." (point-at-bol))
+                               (point)))
+         (end (point))
+         (pattern (buffer-substring-no-properties beg end))
+
+         (completions nil)
+         (completion nil)
+         (comint-preoutput-filter-functions
+          (append comint-preoutput-filter-functions
+                  '(ansi-color-filter-apply
+                    (lambda (string)
+                      (setq ugly-return (concat ugly-return string))
+                      "")))))
+
+    (message pattern)
+    (process-send-string python-process
+                         (format ipython-completion-command-string pattern))
+    (accept-process-output python-process)
+    (setq completions
+          (split-string (substring ugly-return 0 (position ?\n ugly-return)) sep))
+
+    (setq completions (if (string-match "\\." pattern)
+                          (mapcar
+                             (lambda (completion)
+                               (car (last (cdr (split-string completion "\\.")))))
+                             completions)
+                        completions))))
 
 
-
+;; (require 'anything)
+;; (require 'anything-ipython)
+;; (when (require 'anything-show-completion nil t)
+;;    (use-anything-show-completion 'anything-ipython-complete
+;;                                  '(length initial-pattern)))
