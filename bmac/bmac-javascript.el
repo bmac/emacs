bb93c8cf47df7ef2369cc88cfc61d6d43a748202
bmac <b@brendanmcloughlin.com>
2011-12-18 14:37:21 -0500

    using a forked version of js2 mode with better indenting. woot \o/

--- bmac/bmac-javascript.el
+++ bmac/bmac-javascript.el
@@ -1,19 +1,11 @@
 ;;http://steve-yegge.blogspot.com/2008/03/js2-mode-new-javascript-mode-for-emacs.html
-;;emacs --batch --eval '(byte-compile-file "js2-20090723b.el")'
-
-(load "js2-20090723b")
-(setq js2-use-font-lock-faces t)
+;; Now using fork from https://github.com/mooz/js2-mode
+;; emacs --batch -f batch-byte-compile js2-mode.el
+;;(setq js2-use-font-lock-faces t)
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(setq 
- js2-auto-indent-p t
- js2-basic-offset 2
- js2-bounce-indent-p t
- js2-cleanup-whitespace t
- js2-enter-indents-newline t
- js2-highlight-level 3
- js2-indent-on-enter-key t
- js2-mirror-mode nil
- js2-mode-escape-quotes nil
- js2-enter-indents-newline t
- js2-indent-on-enter-key nil)
-
+(setq js2-consistent-level-indent-inner-bracket-p t
+      js2-pretty-multiline-decl-indentation-p t
+      js2-cleanup-whitespace t
+      js2-enter-indents-newline t
+      js2-indent-on-enter-key nil)
