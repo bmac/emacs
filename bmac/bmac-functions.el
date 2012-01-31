780766a57d50f0ec22b58d56b8217886ec2adbfc
bmac <b@brendanmcloughlin.com>
2012-01-30 22:58:22 -0500

    added duplicate line function

--- bmac/bmac-functions.el
+++ bmac/bmac-functions.el
@@ -122,3 +122,14 @@ buffer read-only, so I suggest setting kill-read-only-ok to t."
     (narrow-to-region (line-beginning-position) (line-end-position))
     ;;search foward till you find a , " ) or ;
     (re-search-forward "[,);\"]")))
+
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+)
+(global-set-key (kbd "C-d") 'duplicate-line)
