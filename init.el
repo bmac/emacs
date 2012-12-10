11ebc4a6fb57c8f88340cd65677f9c0f19393b0d
bmac <bmac325@gmail.com>
2012-12-09 23:48:41 -0500

    Do not load the config file when working in emacs -nw mode.

--- init.el
+++ init.el
@@ -1,43 +1,50 @@
-;; Set the load path
-(setq load-path (cons "~/.emacs.d/" load-path))
-(setq load-path (cons "~/.emacs.d/vendor/" load-path))
-(setq custom-theme-load-path (cons "~/.emacs.d/vendor/emacs-color-theme-solarized" custom-theme-load-path))
-(setq load-path (cons "~/.emacs.d/bmac/" load-path))
+;; Run vanilla emacs in the terminal
+(when (display-graphic-p)
 
-;;Make sure .emacs file is edited in lisp mode
-(setq auto-mode-alist (cons '("\.emacs" . lisp-mode) auto-mode-alist))
+  ;; Set the load path
+  (setq load-path (cons "~/.emacs.d/" load-path))
+  (setq load-path (cons "~/.emacs.d/vendor/" load-path))
+  (setq custom-theme-load-path (cons "~/.emacs.d/vendor/emacs-color-theme-solarized" custom-theme-load-path))
+  (setq load-path (cons "~/.emacs.d/bmac/" load-path))
 
-
-;;bmac.el stuff is custom setting of 
-;;various libs 
-
-;; one off settings and key assignments
-(load "bmac-settings.el")
-
-;;functions I find useful
-(load "bmac-functions.el")
-
-;;use hippie expand instead of debbrev
-(load "bmac-hippie-expand.el")
-
-(load "bmac-javascript.el")
-
-(require 'browse-kill-ring)
-
-(load "bmac-abbrev-table.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fringe-mode 0 nil (fringe))
- '(ns-pop-up-frames nil))
+  ;;Make sure .emacs file is edited in lisp mode
+  (setq auto-mode-alist (cons '("\.emacs" . lisp-mode) auto-mode-alist))
 
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  ;;bmac.el stuff is custom setting of
+  ;;various libs
+
+  ;; one off settings and key assignments
+  (load "bmac-settings.el")
+
+  ;;functions I find useful
+  (load "bmac-functions.el")
+
+  ;;use hippie expand instead of debbrev
+  (load "bmac-hippie-expand.el")
+
+  (load "bmac-javascript.el")
+
+  (require 'browse-kill-ring)
+
+  (load "bmac-abbrev-table.el")
+
+  ;;(load-file "~/.emacs.d/vendor/emacs-for-python/epy-init.el")
+
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(fringe-mode 0 nil (fringe))
+   '(ns-pop-up-frames nil))
+
+
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   )
+
+  )
