4c78a977b29c63d121eac20de4bd86e0efddfd12
bmac <bmac325@gmail.com>
2013-04-07 19:17:03 -0400

    add elpa directory to repo

---
+++ elpa/popup-20130324.1305/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (20833 58940
+;;;;;;  36990))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
