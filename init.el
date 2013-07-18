4be13c7ba87e50d63f099ce0a65eba650fa54dfb
Brendan McLoughlin <bmac325@gmail.com>
2013-07-17 21:18:59 -0400

    clean up custom-set-variables

--- init.el
+++ init.el
@@ -36,34 +36,10 @@
 
   ;;(load-file "~/.emacs.d/vendor/emacs-for-python/epy-init.el")
 
-  (custom-set-variables
-   ;; custom-set-variables was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   '(fringe-mode 0 nil (fringe))
-   '(ns-pop-up-frames nil))
-
-
-  (custom-set-faces
-   ;; custom-set-faces was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   )
-
-  )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(fringe-mode 0 nil (fringe))
- '(js2-basic-offset 2)
  '(ns-pop-up-frames nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
