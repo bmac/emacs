114870a186748a213a58abb2baad04b95f4c45d8
bmac <bmac325@GMail.com>
2014-02-08 10:09:59 -0500

    Add markdown mode

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
   (setq load-path (cons "~/.emacs.d/bmac/" load-path))
   
   ;; use a package manager
-  (setq prelude-packages '(cl-lib browse-kill-ring coffee-mode exec-path-from-shell find-file-in-repository git-gutter-fringe less-css-mode magit js2-mode solarized-theme jedi))
+  (setq prelude-packages '(cl-lib browse-kill-ring coffee-mode exec-path-from-shell find-file-in-repository git-gutter-fringe less-css-mode magit js2-mode solarized-theme jedi markdown-mode))
   (require 'prelude-packages)
   
 
