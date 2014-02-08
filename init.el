a271492aab115f4043ff1827f73fcfa9b895f31a
bmac <bmac325@GMail.com>
2014-02-08 10:10:50 -0500

    Remove less css mode

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
   (setq load-path (cons "~/.emacs.d/bmac/" load-path))
   
   ;; use a package manager
-  (setq prelude-packages '(cl-lib browse-kill-ring exec-path-from-shell find-file-in-repository git-gutter-fringe less-css-mode magit js2-mode solarized-theme jedi markdown-mode))
+  (setq prelude-packages '(cl-lib browse-kill-ring exec-path-from-shell find-file-in-repository git-gutter-fringe magit js2-mode solarized-theme jedi markdown-mode))
   (require 'prelude-packages)
   
 
