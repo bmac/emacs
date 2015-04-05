98b814f95d6963f2504513b1cfd7ff10aba19298
bmac <bmac325@GMail.com>
2015-04-04 21:49:48 -0400

    Use flx ido

--- bmac/bmac-settings.el
+++ bmac/bmac-settings.el
@@ -135,3 +135,12 @@
 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
+
+
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
