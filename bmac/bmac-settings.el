966bcaca25fab21937a195d04f3dd3e0836016c2
bmac <bmac325@gmail.com>
2016-04-02 20:12:10 -0400

    Make copy and paste work as expected in the terminal

--- bmac/bmac-settings.el
+++ bmac/bmac-settings.el
@@ -148,3 +148,16 @@
 
 
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . html-mode))
+
+
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
