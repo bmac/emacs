47dbd5b0c72d53e1660d352f76f0ba76867a11bf
bmac <bmac325@gmail.com>
2016-11-13 09:46:58 -0500

    Remove unused functions

--- init.el
+++ init.el
@@ -52,22 +52,4 @@
  ;; If there is more than one, they won't work right.
  )
 
-(defun pbcopy ()
-  (interactive)
-  (call-process-region (point) (mark) "pbcopy")
-  (setq deactivate-mark t))
-
-(defun pbpaste ()
-  (interactive)
-  (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
-
-(defun pbcut ()
-  (interactive)
-  (pbcopy)
-  (delete-region (region-beginning) (region-end)))
-
-(global-set-key (kbd "C-c c") 'pbcopy)
-(global-set-key (kbd "C-c v") 'pbpaste)
-(global-set-key (kbd "C-c x") 'pbcut)
-
 (setq create-lockfiles nil)
