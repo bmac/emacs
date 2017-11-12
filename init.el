22576a8bc71ffdf4bb1d4bc7868add88618b5dd7
bmac <bmac325@gmail.com>
2017-11-11 22:54:48 -0500

    Remove abbrev table

--- init.el
+++ init.el
@@ -34,7 +34,6 @@
 
 (load "bmac-javascript.el")
 
-(load "bmac-abbrev-table.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
