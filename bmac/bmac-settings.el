e6acab1dca4cf0217ef523518d326d7c720586f9
bmac <bmac325@gmail.com>
2018-08-01 12:47:22 -0400

    use markdown mode

--- bmac/bmac-settings.el
+++ bmac/bmac-settings.el
@@ -198,3 +198,11 @@
   (setq powerline-display-mule-info nil)
   (powerline-default-theme)
   )
+
+
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
