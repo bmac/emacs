0eb6d8c025c820d9cc79500f5dbae3e072e9b0e7
bmac <bmac325@gmail.com>
2023-12-28 18:52:55 -0500

    add new packages

---
+++ elpa/mocha-20200729.1130/mocha-pkg.el
@@ -0,0 +1,15 @@
+(define-package "mocha" "20200729.1130" "Run Mocha or Jasmine tests"
+  '((js2-mode "20150909")
+    (f "0.18"))
+  :commit "6a72fa20e7be6e55c09b1bc9887ee09c5df28e45" :authors
+  '(("Al Scott"))
+  :maintainers
+  '(("Al Scott"))
+  :maintainer
+  '("Al Scott")
+  :keywords
+  '("javascript" "mocha" "jasmine")
+  :url "http://github.com/scottaj/mocha.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
